8eb97a708f26765ba589f4429458f0bc463a3c4e
TheSkiF4er <cajeerskif4er@gmail.com>
2025-12-11 15:44:31 +0300

    Add files via upload

--- stdlib/fs/mod.ts
+++ stdlib/fs/mod.ts
@@ -1,1 +1,7 @@
-
+/**
+ * Простейшая заглушка файловых операций Farog.
+ */
+export function readTextFile(path: string): Promise<string> {
+  console.log(`farog/fs.readTextFile('${path}') — заглушка.`);
+  return Promise.resolve("");
+}
