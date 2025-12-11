d86b3b2889184664ee26c354d8d943b015aac40c
TheSkiF4er <cajeerskif4er@gmail.com>
2025-12-11 15:33:12 +0300

    Add files via upload

--- examples/starter-http/src/index.ts
+++ examples/starter-http/src/index.ts
@@ -1,1 +1,6 @@
+import { serve } from "farog/http";
 
+serve({ port: 8080 }, async (request) => {
+  const url = new URL(request.url);
+  return new Response(`Farog starter-http: ${url.pathname}`);
+});
